06ccb40b2a3c190b9e0dfdfe1fa91bcf7a3c762c
Guilhem <guilhem.dubois@polymtl.ca>
2020-12-22 12:32:23 -0500

    removing contact footnotes from contact page and adjust icons

---
+++ src/app/ui-tools/footnotes/footnotes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footnotes',
+  templateUrl: './footnotes.component.html',
+  styleUrls: ['./footnotes.component.scss']
+})
+export class FootnotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/ui-tools/ui-tools.module.ts
+++ src/app/ui-tools/ui-tools.module.ts
@@ -4,10 +4,11 @@ import { BackButtonComponent } from './back-button/back-button.component';
 import { MenuComponent } from './menu/menu.component';
 import { MenuItemComponent } from './menu-item/menu-item.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { FootnotesComponent } from './footnotes/footnotes.component';
 
 @NgModule({
-	declarations: [BackButtonComponent, MenuComponent, MenuItemComponent],
+	declarations: [BackButtonComponent, MenuComponent, MenuItemComponent, FootnotesComponent],
 	imports: [CommonModule, AppRoutingModule],
-	exports: [BackButtonComponent, MenuComponent, MenuItemComponent],
+	exports: [BackButtonComponent, MenuComponent, MenuItemComponent, FootnotesComponent],
 })
 export class UiToolsModule {}
