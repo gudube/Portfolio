e8f94d7e001b2f74346d11929959bb9727340798
Guilhem <guilhem.dubois@polymtl.ca>
2021-01-08 17:03:29 -0500

    set up project for i18n

---
+++ src/app/ui-tools/locale-selector/locale-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-locale-selector',
+  templateUrl: './locale-selector.component.html',
+  styleUrls: ['./locale-selector.component.scss']
+})
+export class LocaleSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/ui-tools/ui-tools.module.ts
+++ src/app/ui-tools/ui-tools.module.ts
@@ -5,10 +5,11 @@ import { MenuComponent } from './menu/menu.component';
 import { MenuItemComponent } from './menu-item/menu-item.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { FootnotesComponent } from './footnotes/footnotes.component';
+import { LocaleSelectorComponent } from './locale-selector/locale-selector.component';
 
 @NgModule({
-	declarations: [BackButtonComponent, MenuComponent, MenuItemComponent, FootnotesComponent],
+	declarations: [BackButtonComponent, MenuComponent, MenuItemComponent, FootnotesComponent, LocaleSelectorComponent],
 	imports: [CommonModule, AppRoutingModule],
-	exports: [BackButtonComponent, MenuComponent, MenuItemComponent, FootnotesComponent],
+	exports: [BackButtonComponent, MenuComponent, MenuItemComponent, FootnotesComponent, LocaleSelectorComponent],
 })
 export class UiToolsModule {}
