a0a0be7a420349e5abdf054fbf8456603bb1c935
Guilhem <guilhem.dubois@polymtl.ca>
2021-01-19 14:47:40 -0500

    adding a go to top button

--- src/app/ui-tools/back-button/back-button.component.ts
+++ src/app/ui-tools/back-button/back-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { ROUTES } from '../../app-routing.module';
 
@@ -13,9 +13,24 @@ export class BackButtonComponent implements OnInit {
 	public opened = false;
 	public currentRoute = '';
 	public routes = ROUTES.filter(route => !route.data.personal.hidden);
+	public showGoToTop = false;
 	constructor(private router: Router) {}
 
 	ngOnInit(): void {
 		this.currentRoute = this.router.url;
+	}
+
+	@HostListener('window:scroll', [])
+	private onWindowScroll(): void {
+		const currentScroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+		this.showGoToTop = currentScroll > 800;
+	}
+
+	public scrollToTop(): void {
+		const currentScroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+		if (currentScroll > 0) {
+			window.scrollTo({top: 0, behavior: 'smooth'});
+			//window.scrollTo(0, currentScroll - (currentScroll / 8));
+		}
 	}
 }
