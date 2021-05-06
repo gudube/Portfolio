da14987fab651175a9acb988ddcea754ccda2ac3
Guilhem Dubois <gdubois@prettyobjects.com>
2021-05-06 16:45:44 -0400

    Ajout image presentation projet

--- src/app/project-page/project-page.component.ts
+++ src/app/project-page/project-page.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-project-page',
-  templateUrl: './project-page.component.html',
-  styleUrls: ['./project-page.component.scss']
+	selector: 'app-project-page',
+	templateUrl: './project-page.component.html',
+	styleUrls: ['./project-page.component.scss']
 })
 export class ProjectPageComponent implements OnInit {
 
-  constructor() { }
+	public libraryView = true;
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+
+	public scrollToBottom(element: HTMLElement): void {
+		// window.scrollTo({top: element., behavior: 'smooth'});
+		// element.scrollIntoView({behavior: 'smooth', block: 'start'});
+
+		const y = element.getBoundingClientRect().bottom + window.pageYOffset - 50;
+		window.scrollTo({top: y, behavior: 'smooth'});
+	} 
 
 }
