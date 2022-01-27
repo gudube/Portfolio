797cd57280c19b71c3e5525c54b7d3e8c4e53ff7
Guilhem Dubois <60274480+gudube@users.noreply.github.com>
2022-01-27 15:19:46 -0500

    Dev2 (#52)
    * updated dates
    
    * update bg image about me
    
    * add download github on project page
    
    * update dependencies

--- src/app/about-me-page/about-me-date/about-me-date.component.ts
+++ src/app/about-me-page/about-me-date/about-me-date.component.ts
@@ -10,6 +10,8 @@ export class AboutMeDateComponent {
 	@ViewChild('container') container: ElementRef;
 	@ViewChild('dateSlider') slider: MatSlider;
 
+	@ViewChild('2021') container2021: ElementRef;
+	@ViewChild('2020') container2020: ElementRef;
 	@ViewChild('2019') container2019: ElementRef;
 	@ViewChild('2018') container2018: ElementRef;
 	@ViewChild('2017') container2017: ElementRef;
@@ -19,44 +21,55 @@ export class AboutMeDateComponent {
 	public sliderValue = 1;
 	public labelPos = 0;
 	public disabled = true;
-	public labelYear = '20';
+	public labelYear = '22';
 
 	constructor() { }
 
 	@HostListener('window:scroll', ['$event'])
-	private updateSliderPos() {
+	private updateSliderPos(): void {
 		const positionPx = window.pageYOffset - this.container.nativeElement.offsetTop;
-		if(positionPx < 0){
+		if (positionPx < 0){
 			this.sliderValue = 1;
 			this.labelPos = 0;
 			this.disabled = true;
-			this.labelYear = '20';
+			this.labelYear = '22';
 			return;
 		}
 
 		const fullHeight = this.container.nativeElement.scrollHeight - innerHeight;
 		let percentage = positionPx / fullHeight;
 		this.sliderValue = 1 - percentage;
-		if(percentage > 1){
+		if (percentage > 1){
 			percentage = 1;
 		}
 		this.labelPos = percentage * (this.slider._elementRef.nativeElement.scrollHeight - 17);
 		this.disabled = false;
 
-		if(window.pageYOffset < this.container2019.nativeElement.offsetTop - innerHeight / 1.5)
+		if (window.scrollY < this.container2021.nativeElement.offsetTop - innerHeight / 1.5) {
+			this.labelYear = '22';
+		}
+		else if (window.scrollY < this.container2020.nativeElement.offsetTop - innerHeight / 1.5) {
+			this.labelYear = '21';
+		}
+		else if (window.scrollY < this.container2019.nativeElement.offsetTop - innerHeight / 1.5) {
 			this.labelYear = '20';
-		else if(window.pageYOffset < this.container2018.nativeElement.offsetTop - innerHeight / 1.5)
+		}
+		else if (window.scrollY < this.container2018.nativeElement.offsetTop - innerHeight / 1.5) {
 			this.labelYear = '19';
-		else if(window.pageYOffset < this.container2017.nativeElement.offsetTop - innerHeight / 1.5)
+		}
+		else if (window.scrollY < this.container2017.nativeElement.offsetTop - innerHeight / 1.5) {
 			this.labelYear = '18';
-		else if(window.pageYOffset < this.container2016.nativeElement.offsetTop - innerHeight / 1.5)
+		}
+		else if (window.scrollY < this.container2016.nativeElement.offsetTop - innerHeight / 1.5) {
 			this.labelYear = '17';
-		else
+		}
+		else {
 			this.labelYear = '16';
+		}
 	}
 	public updateScrollPos(value: number): void {
 		const fullHeight = this.container.nativeElement.scrollHeight;
-		const positionPx = (1-value) * fullHeight + this.container.nativeElement.offsetTop;
+		const positionPx = (1 - value) * fullHeight + this.container.nativeElement.offsetTop;
 		window.scrollTo(0, positionPx);
 	}
 }
