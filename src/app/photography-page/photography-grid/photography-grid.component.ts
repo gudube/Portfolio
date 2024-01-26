8335818de741ef44085f1862e51f3040d34c1654
Guilhem Dubois <gdubois@prettyobjects.com>
2024-01-25 23:04:40 -0500

    add hash compatibility so back button closes the image, or reopen it

--- src/app/photography-page/photography-grid/photography-grid.component.ts
+++ src/app/photography-page/photography-grid/photography-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input, OnDestroy, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { FullImageViewerComponent } from '../full-image-viewer/full-image-viewer.component';
 import { IAlbum } from '../models/album.model';
 import { VideoPlayerComponent } from '../video-player/video-player.component';
@@ -8,7 +8,7 @@ import { VideoPlayerComponent } from '../video-player/video-player.component';
 	templateUrl: './photography-grid.component.html',
 	styleUrls: ['./photography-grid.component.scss'],
 })
-export class PhotographyGridComponent implements OnInit, OnDestroy {
+export class PhotographyGridComponent implements AfterViewInit, OnDestroy {
 	public readonly VideoPlayerComponent: typeof VideoPlayerComponent = VideoPlayerComponent;
 
 	@Input()
@@ -23,10 +23,14 @@ export class PhotographyGridComponent implements OnInit, OnDestroy {
 		if (newValue) {
 			document.body.style.overflow = 'hidden';
 			this.gridVideos.forEach(x => x.nativeElement.pause());
+			if (location.hash != '#selected')
+				history.pushState(null, null, location.pathname + '#selected');
 		}
 		else {
 			document.body.style.overflow = 'auto';
 			this.gridVideos.forEach(x => x.nativeElement.play());
+			if (location.hash == '#selected')
+				history.pushState(null, null, location.pathname);
 		}
 		this._showingFullImage = newValue;
 	}
@@ -42,18 +46,35 @@ export class PhotographyGridComponent implements OnInit, OnDestroy {
 	public hasNextImage = false;
 	public gridView = true;
 
-	private selectedFileName: string;
+	private selectedIndex: number;
 
-	ngOnInit(): void {
+	ngAfterViewInit(): void {
+		this.onHashChange();
 	}
 
 	ngOnDestroy(): void {
 		document.body.style.overflow = 'auto';
 	}
 
-	public selectImage(index: number): void{
-		this.selectedFileName = this.album.photoFileNames[index];
-		this.imageViewer.setImage(this.album.id, this.selectedFileName);
+	@HostListener('window:hashchange')
+	private onHashChange() {
+		if(location.hash == '')
+			this.showingFullImage = false;
+		else if(location.hash == '#selected') {
+			if(this.selectedIndex) {
+				this.selectImage(this.selectedIndex);
+			} else {
+				history.replaceState(null, null, location.pathname);
+				this.showingFullImage = false;
+			}
+		}
+	}
+
+	public selectImage(index: number): void {
+		if(this.selectedIndex < 0 || this.selectedIndex >= this.album.photoFileNames.length)
+			return;
+		this.selectedIndex = index;
+		this.imageViewer.setImage(this.album.id, this.album.photoFileNames[index]);
 
 		this.hasPreviousImage = index > 0;
 		this.hasNextImage = index < this.album.photoFileNames.length - 1;
@@ -61,10 +82,7 @@ export class PhotographyGridComponent implements OnInit, OnDestroy {
 	}
 
 	public nextImage(next: boolean): void{
-		const currentId = this.album.photoFileNames.indexOf(this.selectedFileName);
-		const newId = next ? currentId + 1 : currentId - 1;
-		if (newId >= 0 && newId < this.album.photoFileNames.length) {
-			this.selectImage(newId, );
-		}
+		const newId = next ? this.selectedIndex + 1 : this.selectedIndex - 1;
+		this.selectImage(newId);
 	}
 }
